9eaac4fba4878c9b6e3b864d73111f63205ca226
hiro0301 <hiro.gyakutenda.0301@icloud.com>
2020-10-26 23:20:05 +0900

    answer_section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,11 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(100,100);
+  noFill();
+  stroke(0,0,255);
+  for(let x=5;x<25; x=x+5){ellipse(50,50,x)
   }
+  stroke(255,0,0);
+  for(let x=25;x<50; x=x+5){ellipse(50,50,x)
+}
 }
